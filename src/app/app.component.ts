f19a5cf3f1477dee3b82effc59559056d378fef5
Leksip <Nickita.rom@gmail.com>
2022-09-24 20:25:18 +0300

    Добавлена визуализация ошибок и вывод сообщения с ошибкой, автоматический выбор столицы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  form : FormGroup;
-  ngOnInit(){
+  form: FormGroup;
+  title: string = 'ngForm'
 
-    this.form  = new FormGroup({
+  ngOnInit() {
+
+    this.form = new FormGroup({
       email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl('', [Validators.required, Validators.minLength(3)])
-
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      adress: new FormGroup({
+        country: new FormControl('ru'),
+        city: new FormControl('', Validators.required)
+      })
     })
   }
 
   onSubmit() {
-    if(this.form.valid){
+    if (this.form.valid) {
       console.log(this.form)
     }
+  }
+
+  setCapital() {
+    const capitals: any = {
+      ru: 'Москва',
+      ua: 'Киев',
+      by: 'Минск'
+    }
+    const cityKey = this.form.get('adress')?.get('country')?.value
+    const city = capitals[cityKey]
+    this.form.patchValue({adress:{city}})
   }
 }
